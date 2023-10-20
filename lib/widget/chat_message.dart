2c89bb1dd975e5f04ac6624084c0986dea02dd46
= <=>
2023-10-20 12:01:34 +0900

    테스트4

--- lib/widget/chat_message.dart
+++ lib/widget/chat_message.dart
@@ -9,6 +9,32 @@ class ChatMessage extends StatelessWidget {
   ChatMessage(this.data, {Key? key, required this.animation, required this.isMe})
       : super(key: key);
 
+  String formatMessageTime(String timeString) {
+    final DateTime now = DateTime.now();
+    final DateTime messageTime = DateTime.parse(timeString);
+
+    // 현재 시간과의 차이 계산
+    final Duration difference = now.difference(messageTime);
+
+    if (difference.inDays < 1) {
+      // 오늘
+      final String formattedTime = '${messageTime.hour}:${messageTime.minute}';
+      return formattedTime;
+    } else if (difference.inDays < 2) {
+      // 어제
+      final String formattedTime = 'Yesterday ${messageTime.hour}:${messageTime.minute}';
+      return formattedTime;
+    } else if (now.year == messageTime.year) {
+      // 올해
+      final String formattedTime = '${messageTime.month}/${messageTime.day} ${messageTime.hour}:${messageTime.minute}';
+      return formattedTime;
+    } else {
+      // 1년 이전
+      final String formattedTime = '${messageTime.year}/${messageTime.month}/${messageTime.day} ${messageTime.hour}:${messageTime.minute}';
+      return formattedTime;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return FadeTransition(
@@ -32,19 +58,42 @@ class ChatMessage extends StatelessWidget {
               ),
               Expanded(
                 child: Column(
-
                   crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
                   children: [
+                    isMe ?
+                    SizedBox()
+                        :
                     Text(
                       data['userId'],
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Text(data['message'],
+                    Container(
+                      decoration: BoxDecoration(
+                        color: isMe? Colors.yellowAccent:Colors.black54,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                          bottomLeft: isMe ? Radius.circular(10) : Radius.circular(0),
+                          bottomRight: isMe ? Radius.circular(0) : Radius.circular(10),
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        data['message'],
                         style: TextStyle(
                           fontSize: 16,
-                        )),
+                          color: isMe?Colors.black:Colors.white,
+                        ),
+                      ),
+                    ),
+                    Text(
+                        formatMessageTime(data['time']),
+                        style: TextStyle(
+                          fontSize: 11,
+                        )
+                    ),
                   ],
                 ),
               ),
